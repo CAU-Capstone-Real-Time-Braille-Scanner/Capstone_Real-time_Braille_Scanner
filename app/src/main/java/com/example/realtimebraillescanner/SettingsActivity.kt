6d08751b185830e6a5dc6fe5cc881c28085c148c
HyunJinNo <nhj9831@naver.com>
2022-09-29 15:42:48 +0900

    환경설정 화면 추가

---
+++ app/src/main/java/com/example/realtimebraillescanner/SettingsActivity.kt
@@ -0,0 +1,40 @@
+package com.example.realtimebraillescanner
+
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.PreferenceFragmentCompat
+import com.example.realtimebraillescanner.databinding.SettingsActivityBinding
+
+class SettingsActivity : AppCompatActivity() {
+    private lateinit var binding: SettingsActivityBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = SettingsActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.settings, SettingsFragment())
+                .commit()
+        }
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        android.R.id.home -> {
+            onBackPressed()
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
+    }
+
+    class SettingsFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.settings_preference, rootKey)
+        }
+    }
+}
