eb05b7ffba7ec40a25792d54d6837d5142000cbc
hkjeong2 <chali0330@gmail.com>
2022-10-02 20:53:34 +0900

    CameraFragment, MainViewModel 추가

--- app/src/main/java/com/example/realtimebraillescanner/MainActivity.kt
+++ app/src/main/java/com/example/realtimebraillescanner/MainActivity.kt
@@ -8,17 +8,17 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.example.realtimebraillescanner.databinding.ActivityMainBinding
+import com.example.realtimebraillescanner.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: MainActivityBinding
     private var REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
     private val PERMISSIONS_REQUEST_CODE = 100
     private var permissionToCamera = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = MainActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         initClickListener()
@@ -51,7 +51,7 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, "설정에서 카메라 권한을 허용해주세요", Toast.LENGTH_LONG).show()
         }
         else{
-            startActivity(Intent(this, CameraActivity::class.java))
+            startActivity(Intent(this, CameraFragment::class.java))
         }
     }
 
@@ -62,7 +62,7 @@ class MainActivity : AppCompatActivity() {
 
         if (hasCameraPermission == PackageManager.PERMISSION_GRANTED) {
             // 2. 이미 퍼미션을 가지고 있다면 액티비티 이동
-            startActivity(Intent(this, CameraActivity::class.java))
+            startActivity(Intent(this, CameraFragment::class.java))
         } else {  //2. 퍼미션 요청을 허용한 적이 없다면 퍼미션 요청이 필요합니다. 2가지 경우(3-1, 4-1)가 있습니다.
             // 3-1. 사용자가 퍼미션 거부를 한 적이 있는 경우에는
             if (ActivityCompat.shouldShowRequestPermissionRationale(this@MainActivity, REQUIRED_PERMISSIONS.get(0))) {
