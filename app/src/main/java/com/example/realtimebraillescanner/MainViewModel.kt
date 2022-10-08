f4854cb78ef0ee4454c0fa990ed1935f7a891274
hkjeong2 <chali0330@gmail.com>
2022-10-08 22:28:57 +0900

    srcText가 translatedText 출력되도록

--- app/src/main/java/com/example/realtimebraillescanner/MainViewModel.kt
+++ app/src/main/java/com/example/realtimebraillescanner/MainViewModel.kt
@@ -51,27 +51,28 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
     // the actual aspect ratio of images.
     val imageCropPercentages = MutableLiveData<Pair<Int, Int>>()
         .apply { value = Pair(DESIRED_HEIGHT_CROP_PERCENT, DESIRED_WIDTH_CROP_PERCENT) }
-    val translatedText = MediatorLiveData<ResultOrError>()
-    private val translating = MutableLiveData<Boolean>()
+    val translatedText = SmoothedMutableLiveData<String>(SMOOTHING_DURATION)
+//    val translatedText = MediatorLiveData<ResultOrError>()
+//    private val translating = MutableLiveData<Boolean>()
 //    val modelDownloading = SmoothedMutableLiveData<Boolean>(SMOOTHING_DURATION)
 
 //    private var modelDownloadTask: Task<Void> = Tasks.forCanceled()
 
-    private val translators =
-        object : LruCache<TranslatorOptions, Translator>(NUM_TRANSLATORS) {
-            override fun create(options: TranslatorOptions): Translator {
-                return Translation.getClient(options)
-            }
-
-            override fun entryRemoved(
-                evicted: Boolean,
-                key: TranslatorOptions,
-                oldValue: Translator,
-                newValue: Translator?
-            ) {
-                oldValue.close()
-            }
-        }
+//    private val translators =
+//        object : LruCache<TranslatorOptions, Translator>(NUM_TRANSLATORS) {
+//            override fun create(options: TranslatorOptions): Translator {
+//                return Translation.getClient(options)
+//            }
+//
+//            override fun entryRemoved(
+//                evicted: Boolean,
+//                key: TranslatorOptions,
+//                oldValue: Translator,
+//                newValue: Translator?
+//            ) {
+//                oldValue.close()
+//            }
+//        }
 
 //    val sourceLang = Transformations.switchMap(sourceText) { text ->
 //        val result = MutableLiveData<Language>()
@@ -90,7 +91,7 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
     override fun onCleared() {
         // TODO Shut down ML Kit clients.
 //        languageIdentifier.close()
-        translators.evictAll()
+//        translators.evictAll()
     }
 
 
@@ -139,27 +140,27 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
 //    val availableLanguages: List<Language> = TranslateLanguage.getAllLanguages()
 //        .map { Language(it) }
 
-    init {
-//        modelDownloading.setValue(false)
-        translating.value = false
-        // Create a translation result or error object.
-        val processTranslation =
-            OnCompleteListener<String> { task ->
-                if (task.isSuccessful) {
-                    translatedText.value = ResultOrError(task.result, null)
-                } else {
-                    if (task.isCanceled) {
-                        // Tasks are cancelled for reasons such as gating; ignore.
-                        return@OnCompleteListener
-                    }
-                    translatedText.value = ResultOrError(null, task.exception)
-                }
-            }
-        // Start translation if any of the following change: detected text, source lang, target lang.
-//        translatedText.addSource(sourceText) { translate().addOnCompleteListener(processTranslation) }
-//        translatedText.addSource(sourceLang) { translate().addOnCompleteListener(processTranslation) }
-//        translatedText.addSource(targetLang) { translate().addOnCompleteListener(processTranslation) }
-    }
+//    init {
+////        modelDownloading.setValue(false)
+//        translating.value = false
+//        // Create a translation result or error object.
+//        val processTranslation =
+//            OnCompleteListener<String> { task ->
+//                if (task.isSuccessful) {
+//                    translatedText.value = ResultOrError(task.result, null)
+//                } else {
+//                    if (task.isCanceled) {
+//                        // Tasks are cancelled for reasons such as gating; ignore.
+//                        return@OnCompleteListener
+//                    }
+//                    translatedText.value = ResultOrError(null, task.exception)
+//                }
+//            }
+//        // Start translation if any of the following change: detected text, source lang, target lang.
+////        translatedText.addSource(sourceText) { translate().addOnCompleteListener(processTranslation) }
+////        translatedText.addSource(sourceLang) { translate().addOnCompleteListener(processTranslation) }
+////        translatedText.addSource(targetLang) { translate().addOnCompleteListener(processTranslation) }
+//    }
 
     companion object {
         // Amount of time (in milliseconds) to wait for detected text to settle
